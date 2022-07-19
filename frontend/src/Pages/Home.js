3387914351eee979e040fe6bdebe43cba56eedbf
plojr <ufvpaulo@gmail.com>
2022-07-19 00:04:21 -0300

    Uso da API no componente Home

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
+import axios from "axios";
 
+const baseURL = "http://localhost:8081/eldorado/api/v1/";
 function Home() {
+    const [mensagem, setMensagem] = useState("");
+    useEffect(()=> {
+        axios.get(baseURL).then(res => {
+            const msg = res.data;
+            setMensagem(msg);
+        })
+    }, []);
     return (
         <div className="container-fluid">
 	        <div className="row flex-nowrap">
                 <Sidebar />
                 <div className="col py-3">
-                    <p>Bem vindo ao Eldorado!</p>
+                    <p>{mensagem.mensagem}</p>
                 </div>
             </div>
         </div>
