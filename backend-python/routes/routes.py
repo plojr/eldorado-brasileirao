334236275cdc2c3debe3bcf2489f65ffba5bfed5
plojr <ufvpaulo@gmail.com>
2022-07-21 07:35:41 -0300

    Alteração na estrutura das pastas

--- backend-python/routes/routes.py
+++
@@ -1,8 +0,0 @@
-import home_route
-import partida_route
-from flask import Flask
-
-app = Flask(__name__)
-
-app.register_blueprint(home_route.blueprint)
-app.register_blueprint(partida_route.blueprint)
